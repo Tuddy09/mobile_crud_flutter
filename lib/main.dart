db648e104b58b3ec3eada21734674bd6552f8c7a
tuddy <olartudor12@gmail.com>
2024-11-03 11:58:37 +0200

    Added details view and logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_crud_flutter/views/read_view.dart';
 import 'package:provider/provider.dart';
 import 'viewmodels/car_maintenance_viewmodel.dart';
 import 'views/home_view.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -17,7 +20,11 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: HomeView(), // Set HomeView as the starting screen
+        initialRoute: '/',
+        routes: {
+          '/': (context) => HomeView(),
+          '/details': (context) => MaintenanceRecordDetailsScreen(),
+        },
       ),
     );
   }
---
+++ lib/views/read_view.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../viewmodels/car_maintenance_viewmodel.dart';
+
+class MaintenanceRecordDetailsScreen extends StatelessWidget {
+  const MaintenanceRecordDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final id = ModalRoute.of(context)!.settings.arguments as String;
+    final viewModel = Provider.of<CarMaintenanceViewModel>(context);
+    final record = viewModel.getRecord(id);
+
+    return Scaffold(
+      backgroundColor: const Color(0xFF040633),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Card(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "Car Model: ${record?.carModel}",
+                    style: const TextStyle(fontSize: 20, color: Colors.black),
+                  ),
+                  Text(
+                    "Service Type: ${record?.serviceType}",
+                    style: const TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                  Text(
+                    "Service Date: ${record?.serviceDate}",
+                    style: const TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                  Text(
+                    "Service Notes: ${record?.serviceNotes}",
+                    style: const TextStyle(fontSize: 14, color: Colors.black),
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, "update/${record?.id}");
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue),
+                        child: const Text("Update"),
+                      ),
+                      const SizedBox(width: 8),
+                      ElevatedButton(
+                        onPressed: () {
+                          _showDeleteDialog(context, viewModel, record!.id);
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.red),
+                        child: const Text("Delete"),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _showDeleteDialog(
+      BuildContext context, CarMaintenanceViewModel viewModel, String id) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Confirm Delete"),
+          content: const Text("Are you sure you want to delete this record?"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                viewModel.deleteRecord(id);
+                Navigator.of(context).pop(); // Close the dialog
+                Navigator.of(context).pop(); // Go back to the previous screen
+              },
+              child: const Text("Delete", style: TextStyle(color: Colors.red)),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text("Cancel"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
